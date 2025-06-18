c932bd246305978da043f93fd2117eec01d2b764
tacherasasi <tacherasasi@gmail.com>
2025-06-18 15:56:31 +0300

    Add ellie formula

--- Formula/ellie.rb
+++ Formula/ellie.rb
@@ -3,7 +3,7 @@ class Ellie < Formula
   homepage "https://github.com/tacheraSasi/ellie"
   url "https://github.com/tacheraSasi/ellie/releases/download/0.0.91/ellie"
   version "0.0.91"
-  sha256 "PUT_SHA256_CHECKSUM_HERE"
+  sha256 "d865157d1137c07f6ed98f9aec70c0815fe2608174eba18bf23315438589afb2"
 
   def install
     bin.install "ellie"
